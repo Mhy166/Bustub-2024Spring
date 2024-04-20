27162cd022225cfeda0f342f0f391676cc237814
Mhy166 <162128741+Mhy166@users.noreply.github.com>
2024-04-20 22:57:49 +0800

    P4-T2 Completed!

--- src/execution/execution_common.cpp
+++ src/execution/execution_common.cpp
@@ -1,7 +1,13 @@
 #include "execution/execution_common.h"
+#include <cstdint>
+#include <cstdio>
+#include <optional>
+#include <vector>
 #include "catalog/catalog.h"
 #include "common/config.h"
 #include "common/macros.h"
+#include "common/rid.h"
+#include "concurrency/transaction.h"
 #include "concurrency/transaction_manager.h"
 #include "fmt/core.h"
 #include "storage/table/table_heap.h"
@@ -12,20 +18,125 @@ namespace bustub {
 
 auto ReconstructTuple(const Schema *schema, const Tuple &base_tuple, const TupleMeta &base_meta,
                       const std::vector<UndoLog> &undo_logs) -> std::optional<Tuple> {
-  UNIMPLEMENTED("not implemented");
+  std::optional<Tuple> tuple;
+  std::optional<Tuple> sub_tuple=base_tuple;
+  if(base_meta.is_deleted_){
+    tuple=std::nullopt;
+  }
+  else{
+    tuple=base_tuple;
+  }
+  for(const auto& log_iter:undo_logs){
+    if(log_iter.is_deleted_){
+      tuple=std::nullopt;
+      continue;
+    }
+    std::vector<uint32_t> modif;
+    for(uint32_t i=0;i<log_iter.modified_fields_.size();i++){
+      if(log_iter.modified_fields_[i]){//重建
+        modif.push_back(i);
+      }
+    }
+    auto par_schema=bustub::Schema::CopySchema(schema, modif);
+    std::vector<Value> tmp;
+    for(uint32_t i=0,j=0;i<log_iter.modified_fields_.size();i++){
+      if(log_iter.modified_fields_[i]){//重建
+        tmp.push_back(log_iter.tuple_.GetValue(&par_schema, j));
+        j++;
+      }
+      else{
+        tmp.push_back(sub_tuple->GetValue(schema,i));
+      }
+    }
+    tuple=Tuple(tmp,schema);
+    sub_tuple=tuple;
+  }
+  return tuple;
 }
 
 void TxnMgrDbg(const std::string &info, TransactionManager *txn_mgr, const TableInfo *table_info,
                TableHeap *table_heap) {
   // always use stderr for printing logs...
+  fmt::println(stderr,"------------------------------------------------");
   fmt::println(stderr, "debug_hook: {}", info);
-
-  fmt::println(
-      stderr,
-      "You see this line of text because you have not implemented `TxnMgrDbg`. You should do this once you have "
-      "finished task 2. Implementing this helper function will save you a lot of time for debugging in later tasks.");
-
-  // We recommend implementing this function as traversing the table heap and print the version chain. An example output
+  fmt::println(stderr,"table_oid:{}",table_info->oid_);
+  fmt::println(stderr," ");
+  auto iter=table_heap->MakeIterator();
+  auto schema=table_info->schema_;
+  for(;!iter.IsEnd();){
+    auto [meta,tuple]=iter.GetTuple();
+    fmt::print(stderr,"|RID={}/{}  |",tuple.GetRid().GetPageId(),tuple.GetRid().GetSlotNum());
+    if(meta.ts_<TXN_START_ID){
+      fmt::print(stderr,"ts={}  |",meta.ts_);
+    }
+    else{
+      fmt::print(stderr,"ts=txn{}  |",meta.ts_-TXN_START_ID);
+    }
+    if(meta.is_deleted_){
+      fmt::print(stderr," <deleted> ");
+    }
+    fmt::print(stderr,"tuple=(");
+    for(uint32_t i=0;i<schema.GetColumns().size();i++){
+      auto value=tuple.GetValue(&schema,i);
+      if(value.IsNull()){
+        fmt::print(stderr,"<NULL>");
+      }
+      else{
+        fmt::print(stderr," {}",value);
+      }
+      if(i!=schema.GetColumns().size()-1){
+        fmt::print(stderr,",");
+      }
+      else{
+        fmt::print(stderr," ");
+      }
+    }
+    fmt::println(stderr,")  |");
+    //输出版本链
+    auto undolink=txn_mgr->GetUndoLink(tuple.GetRid());
+    std::optional<UndoLog> undolog=std::nullopt;
+    if(undolink!=std::nullopt){
+      undolog=txn_mgr->GetUndoLog(undolink.value());
+    }
+    std::vector<UndoLog> remake_log; 
+    while(true){
+      fmt::print(stderr,"  -->txn{}@{} : ",undolink->prev_txn_-TXN_START_ID,undolink->prev_log_idx_);
+      remake_log.push_back(undolog.value());
+      auto remake_tuple=bustub::ReconstructTuple(&schema,tuple,meta,remake_log);
+      if(remake_tuple==std::nullopt){
+        fmt::print(stderr,"is deleted ");
+      }
+      else{
+        fmt::print(stderr,"tuple=(");
+        for(uint32_t i=0;i<schema.GetColumns().size();i++){
+          auto value=remake_tuple->GetValue(&schema,i);
+          if(value.IsNull()){
+            fmt::print(stderr,"<NULL>");
+          }
+          else{
+            fmt::print(stderr," {}",value);
+          }
+          if(i!=schema.GetColumns().size()-1){
+            fmt::print(stderr,",");
+          }
+          else{
+            fmt::print(stderr," ");
+          }
+        }
+        fmt::print(stderr,") ");
+      }
+      fmt::println(stderr,"ts={}",undolog->ts_);
+      if(undolog->prev_version_.prev_txn_==INVALID_TXN_ID){
+        break;
+      }
+      undolink=undolog->prev_version_;
+      undolog=txn_mgr->GetUndoLog(undolink.value());
+    }
+    fmt::println(stderr," ");
+    ++iter;
+  }
+  fmt::println(stderr,"------------------------------------------------");
+    // We recommend implementing this function as traversing the table heap and print the version chain. An example output
   // of our reference solution:
   //
   // debug_hook: before verify scan
--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -12,10 +12,15 @@
 
 #include "execution/executors/seq_scan_executor.h"
 #include <memory>
+#include <optional>
 #include <utility>
+#include <vector>
+#include "common/config.h"
+#include "concurrency/transaction.h"
+#include "execution/execution_common.h"
 #include "storage/table/table_iterator.h"
 #include "storage/table/tuple.h"
-
+#include "concurrency/transaction_manager.h"
 namespace bustub {
 
 SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx),plan_(plan) {}
@@ -30,22 +35,82 @@ auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
         if(iter_->IsEnd()){
             return false;
         }
-        if(iter_->GetTuple().first.is_deleted_){
-            ++(*iter_);
-            continue;
+        //未结束呢，针对某个元组
+        auto [meta_base, tuple_base] = iter_->GetTuple();
+        auto local_ts=GetExecutorContext()->GetTransaction()->GetReadTs();
+        
+        //MVCC
+        bool do_this=false;
+        if(meta_base.ts_<TXN_START_ID){
+            if(meta_base.ts_<=local_ts){//case 1
+                do_this=true;
+            }
         }
-        Tuple tuple_con=(iter_->GetTuple().second);
-        if(expr!=nullptr){
-            if(!expr->Evaluate(&tuple_con,GetOutputSchema()).GetAs<bool>()){
+        else{
+            if(meta_base.ts_==GetExecutorContext()->GetTransaction()->GetTransactionTempTs()){//case 2
+                do_this=true;
+            }
+        }
+        if(do_this){//就处理基元组了
+            if(meta_base.is_deleted_){
                 ++(*iter_);
                 continue;
             }
+            if(expr!=nullptr&&!expr->Evaluate(&tuple_base,GetOutputSchema()).GetAs<bool>()){
+                ++(*iter_);
+                continue;
+            }
+            *rid=tuple_base.GetRid();
+            *tuple=tuple_base;
+            ++(*iter_);
+            return true;
         }
-        break;
+        //case 3 重构版本
+        *rid=tuple_base.GetRid();
+        const auto &txnmgr=GetExecutorContext()->GetTransactionManager();
+        std::optional<UndoLink> undo_link=txnmgr->GetUndoLink(*rid);
+        std::optional<UndoLog> undo_log=std::nullopt;
+        if(undo_link!=std::nullopt){
+            undo_log=txnmgr->GetUndoLog(undo_link.value());
+        }
+        if(undo_log==std::nullopt){//不返回
+            ++(*iter_);
+            continue;
+        }
+        std::vector<UndoLog> res_log;
+        bool flag=false;
+        bool delete_flag=false;
+        while(true){
+            res_log.push_back(*undo_log);
+            if(undo_log->ts_<=local_ts){
+                if(undo_log->is_deleted_){
+                    delete_flag=true;
+                }
+                break;
+            }
+            if(undo_log->prev_version_.prev_txn_==INVALID_TXN_ID){//最后一个
+                if(undo_log->ts_>local_ts){
+                    flag=true;
+                }
+                break;
+            }
+            undo_log=txnmgr->GetUndoLog(undo_log->prev_version_);
+        }
+        if(flag||delete_flag){
+            ++(*iter_);
+            continue;
+        }
+        auto re_ruple=bustub::ReconstructTuple(&GetOutputSchema(),tuple_base,meta_base,res_log);
+        if(re_ruple.has_value()){
+            if(expr!=nullptr&&!expr->Evaluate(&re_ruple.value(),GetOutputSchema()).GetAs<bool>()){
+                ++(*iter_);
+                continue;
+            }
+            *tuple=re_ruple.value();
+            ++(*iter_);
+            return true;
+        }
+        ++(*iter_);
     }
-    *tuple=iter_->GetTuple().second;
-    *rid=iter_->GetRID();
-    ++(*iter_);
-    return true;
 }
 }  // namespace bustub
